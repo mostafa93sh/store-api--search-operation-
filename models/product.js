e47349d791d276069e3658bece0dc47f38e4d460
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-20 14:44:20 +0200

    feat(product): add product model schema
    - Define product schema with name, rating, price
    - Include createdAt and company fields
    - Set default rating and company enum

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "product name must be provided"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "product rating must be provided"],
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  company: {
+    type: String,
+    enum: {
+      value: ["ikea", "marcos", "caressa", "liddy"],
+      message: "{VALUE} not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
