6f498e52d1a7ac6f3bef6f300994049c5605d9c7
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-20 19:26:40 +0200

    refactor(products): reorder express router instantiation
    - Moved router instantiation after import
    - No functional change

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
 const { getAllProducts } = require("../controllers/products");
+const router = express.Router();
 
 router.get("/", getAllProducts);
 
