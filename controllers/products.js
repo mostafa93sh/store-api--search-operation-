b2494be049a014b4daba6047450c47d9c67439ce
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-21 07:01:27 +0200

    feat(products): add query filtering to get all products
    - Implemented filtering by featured status
    - Added filtering by company name
    - Enabled case-insensitive name search

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,17 @@ const getAllProductsStatic = (req, res) => {
   res.status(200).json("sucess ");
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) queryObject.company = company;
+
+  if (name) {
+    queryObject.name = { $regex: name, $option: "i" };
+  }
+  const products = await Product.find(queryObject);
 
   res.status(200).json({ products });
 };
